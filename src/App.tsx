8386cba83cfa869dde39a5f6a03eba5c77fe0c96
Toya-Onodera <gtrzexel@gmail.com>
2022-03-11 17:47:27 +0900

    Add: Prettier を導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import 'aframe';
-import React from 'react';
+import "aframe";
+import React from "react";
 
 // FIXME: 型定義ファイルがなさそうなので自分で定義する必要がありそう？
 // @ts-ignore
@@ -7,16 +7,16 @@ import { Entity, Scene } from "aframe-react";
 
 function App() {
   return (
-      <Scene>
-          <Entity
-              geometry={{ primitive: 'box' }}
-              material={{ color: 'red' }}
-              position={{ x: 0, y: 0, z: -5 }}
-              rotation={{ x: 0, y: 45, z: 45 }}
-              scale={{ x: 2, y: 2, z: 2 }}
-          />
-          <Entity primitive="a-sky" material="color: #ccc" />
-      </Scene>
+    <Scene>
+      <Entity
+        geometry={{ primitive: "box" }}
+        material={{ color: "red" }}
+        position={{ x: 0, y: 0, z: -5 }}
+        rotation={{ x: 0, y: 45, z: 45 }}
+        scale={{ x: 2, y: 2, z: 2 }}
+      />
+      <Entity primitive="a-sky" material="color: #ccc" />
+    </Scene>
   );
 }
 
