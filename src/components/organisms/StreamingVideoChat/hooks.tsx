f38cae2a4fcab1308f7d0952efd1b79d9fabdba3
Toya-Onodera <gtrzexel@gmail.com>
2022-03-22 18:46:36 +0900

    Add: 順番に配置するように実装

--- src/components/organisms/StreamingVideoChat/hooks.tsx
+++ src/components/organisms/StreamingVideoChat/hooks.tsx
@@ -4,7 +4,7 @@ import { AVideoProps } from "../../atoms/AVideoSource";
 import { AVideoStreamProps } from "../../atoms/AVideoStream";
 
 // Contexts
-import { StreamInfo, AllStreamStoreContext } from "../../pages/App";
+import { AllStreamStoreContext } from "../../pages/App";
 import { SEAT_NUMBERS } from "../../../constants/SEAT_NUMBERS";
 
 export const useStreamingVideoChatHooks = () => {
@@ -16,53 +16,33 @@ export const useStreamingVideoChatHooks = () => {
       const videoWidth = "4";
       const videoHeight = "2.25";
       const positions = ["-4 0 0", "0 0 -4", "4 0 0"];
-      const rotations = ["0 45 0", "0 0 0", "0 -45 0"];
+      const rotations = ["0 90 0", "0 0 0", "0 -90 0"];
 
       // MEMO: Null にはならないが、コンパイルエラーが発生してしまっている
       // @ts-ignore
       const { seat: targetPosition }: { seat: number } =
         allStreamStore.localStream;
 
-      // seat の値が格納されている座席のみに配置を行う
-      const seatSeatStreams = allStreamStore.otherStream.filter(
-        (e): e is Required<StreamInfo> => Boolean(e.seat)
-      );
-
-      //console.log(seatSeatStreams);
-      // TODO: roomID を取得したら seat を Firebase から取得する動作を実装する必要がある
-
       // 自分の位置を手前中央とし、他のユーザの配置順を決める
-      const positionSawFromMe = [1, 2, 3]
+      const positionSawFromMe = [1, 2, 3].map((n) => {
+        const position = targetPosition + n;
+        return position <= SEAT_NUMBERS.HOUR
+          ? position
+          : position - SEAT_NUMBERS.HOUR;
+      });
+
+      // 接続していない場所は黒映像を流す
+      return positionSawFromMe
         .map((n) => {
-          const position = targetPosition + n;
-          return position <= SEAT_NUMBERS.HOUR
-            ? position
-            : position - SEAT_NUMBERS.HOUR;
+          return (
+            allStreamStore.otherStream.find(({ seat }) => n === seat) || {
+              stream: null,
+            }
+          );
         })
-        .filter((n) =>
-          seatSeatStreams.some(({ seat }) => {
-            return n === seat;
-          })
-        );
-
-      // const correctionCoordinates = positionSawFromMe.map(
-      //   (positionNumber, i) => {
-      //     return {
-      //       src: allStreamStore.otherStream.find(
-      //         ({ seat }) => seat === positionNumber
-      //       ).stream,
-      //       positions: positions[i],
-      //       rotations: rotations[i],
-      //     };
-      //   }
-      // );
-
-      // @ts-ignore
-      return allStreamStore.otherStream
-        .filter((e): e is Required<StreamInfo> => Boolean(e.seat))
         .map((streamInfo, i) => {
           return {
-            src: streamInfo ? streamInfo?.stream : null,
+            src: streamInfo.stream,
             position: positions[i],
             rotation: rotations[i],
             width: videoWidth,
